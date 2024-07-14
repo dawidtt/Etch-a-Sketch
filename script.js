eda5ac66f5d6df80501d3426fb418c24f4b89d14
dawidtt <dawid.gorszka@gmail.com>
2024-07-14 23:57:15 +0200

    Add css to container nad inner-grid

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createGrid() {
   const container = document.querySelector("#container");
-  for (let i = 0; i < 65; i++) {
+  for (let i = 0; i < 256; i++) {
     console.log("halo");
     const div = document.createElement("div");
     div.classList.add("inner-grid");
