584aad4e9428b78f244bc7e425bc83207181c8d1
dawidtt <dawid.gorszka@gmail.com>
2024-07-15 00:16:43 +0200

    Add drawing

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function createGrid() {
   const container = document.querySelector("#container");
   for (let i = 0; i < 256; i++) {
-    console.log("halo");
     const div = document.createElement("div");
     div.classList.add("inner-grid");
+    div.id = `element-${i}`;
     container.appendChild(div);
+    let currentElement = document.querySelector(`#element-${i}`);
+    currentElement.addEventListener("mouseover", () => {
+      currentElement.style.backgroundColor = "black";
+    });
   }
 }
+
 createGrid();
